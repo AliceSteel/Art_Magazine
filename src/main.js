53a92867235fd674045c61e90552a2ea70e62810
Olga Garkusha <olga.garkusha.uk@gmail.com>
2022-11-21 13:44:15 +0100

    added firebase

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { initializeApp } from 'firebase/app'
+
+import firebaseConfig from '@/utilities/firebase'
+  
+  // Initialize Firebase
+initializeApp(firebaseConfig);
 
 import './assets/sass/index.scss';
 
