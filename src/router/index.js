794181eb7c358f03a2776d66577947fcdad4eb43
Olga Garkusha <olga.garkusha.uk@gmail.com>
2022-12-12 10:22:02 +0100

    renamed chunks

--- src/router/index.js
+++ src/router/index.js
@@ -4,37 +4,37 @@ const routes = [
   {
     path: '/',
     name: 'home',
-    component: () => import('../views/HomeView.vue')
+    component: () => import(/* webpackChunkName: "Home" */'../views/HomeView.vue')
   },
   {
     path: '/magazine',
     name: 'magazine',
-    component: () => import('../views/articles/MagazinePage.vue')
+    component: () => import(/* webpackChunkName: "Magazine" */'../views/articles/MagazinePage.vue')
   },
   {
 		path: '/magazine/:id',
 		name: 'ArticlePage',
-		component: () => import('../views/articles/ArticlePage.vue')
+		component: () => import(/* webpackChunkName: "ArticlePage" */'../views/articles/ArticlePage.vue')
 	},
   {
     path: '/podcasts',
     name: 'podcasts',
-    component: () => import('../views/podcasts/PodcastsPage.vue')
+    component: () => import(/* webpackChunkName: "Podcasts" */'../views/podcasts/PodcastsPage.vue')
   },
   {
     path: '/podcasts/:id',
     name: 'PodcastPage',
-    component: () => import('../views/podcasts/PodcastPage.vue')
+    component: () => import(/* webpackChunkName: "PodcastPage" */'../views/podcasts/PodcastPage.vue')
   },
   {
     path: '/authors',
     name: 'authors',
-    component: () => import('../views/authors/AuthorsPage.vue')
+    component: () => import(/* webpackChunkName: "Authors" */'../views/authors/AuthorsPage.vue')
   },
   {
     path: '/authors/:id',
     name: 'AuthorPage',
-    component: () => import('../views/authors/AuthorPage.vue')
+    component: () => import(/* webpackChunkName: "AuthorPage" */'../views/authors/AuthorPage.vue')
   },
 ]
 
